6a04e18022a6b055e3016f3a8c97570075ae07a8
Osiris Montiel <omontielc@hotmail.com>
2026-06-05 22:33:34 -0600

    refactor: modify the workflow file

--- src/test/java/util/EnvironmentWriter.java
+++ src/test/java/util/EnvironmentWriter.java
@@ -3,13 +3,13 @@ package util;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
 
 /**
  * Writes environment metadata to allure-results/environment.properties
- * so Allure displays it in the ENVIRONMENT section of the report.
  * @author Osiris Montiel Campos
  */
 public class EnvironmentWriter {
@@ -18,6 +18,11 @@ public class EnvironmentWriter {
     private static final String ALLURE_RESULTS_PATH = "target/allure-results/environment.properties";
 
     public static void write(String baseUrl, String environment) {
+
+        // Create the directory if it doesn't exist
+        File file = new File(ALLURE_RESULTS_PATH);
+        file.getParentFile().mkdirs();
+
         Properties props = new Properties();
         props.setProperty("Environment",  environment);
         props.setProperty("Base.URL",     baseUrl);
@@ -26,7 +31,7 @@ public class EnvironmentWriter {
         props.setProperty("Java.Version", System.getProperty("java.version"));
         props.setProperty("OS",           System.getProperty("os.name"));
 
-        try (FileWriter writer = new FileWriter(ALLURE_RESULTS_PATH)) {
+        try (FileWriter writer = new FileWriter(file)) {
             props.store(writer, "Allure Environment");
             logger.info("environment.properties written to {}", ALLURE_RESULTS_PATH);
         } catch (IOException e) {
